64c3a86f264fd1fc40af391ee8d0b80209605aeb
Anatoly <kjluh@mail.ru>
2023-03-29 22:56:11 +0300

    переход из основного мню в меню этапа 1 работает

--- src/main/java/com/example/teamproject/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/example/teamproject/listener/TelegramBotUpdatesListener.java
@@ -3,7 +3,6 @@ package com.example.teamproject.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.CallbackQuery;
-import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.User;
 import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
@@ -16,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -43,13 +43,20 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
                     String data = callbackQuery.data();
                     switch (data) {
                         case "1" -> shelterInfo(chatId, update);
+                        case "инфа о приюте" ->
+                                telegramBot.execute(new SendMessage(chatId, "тут должна быть информация о приюте"));
+                        case "расписание работы" ->
+                                telegramBot.execute(new SendMessage(chatId, " тут должно быть расписание работы приюта и адрес, схему проезда"));
+                        case "рекомендации о ТБ" ->
+                                telegramBot.execute(new SendMessage(chatId, "тут должны быть общие рекомендации о технике безопасности на территории приюта"));
+                        case "записать данные" ->
+                                telegramBot.execute(new SendMessage(chatId, "тут должен быть метод записать данные"));
                         case "2" -> telegramBot.execute(new SendMessage(chatId, "что-то делаем2"));
                         case "3" -> telegramBot.execute(new SendMessage(chatId, "что-то делаем3"));
                         case "4" -> telegramBot.execute(new SendMessage(chatId, "что-то делаем4"));
                     }
                     return;
                 }
-
 
 
                 User user = update.message().from();
@@ -66,7 +73,9 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
                     button3.callbackData("3");
                     InlineKeyboardButton button4 = new InlineKeyboardButton("Позвать волонтера");
                     button4.callbackData("4");
-                    Keyboard keyboard = new InlineKeyboardMarkup(button1, button2, button3, button4);
+                    InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup();
+                    keyboard.addRow(button1, button2);
+                    keyboard.addRow(button3, button4);
                     helloMessage.replyMarkup(keyboard);
                     telegramBot.execute(helloMessage);
                 }
@@ -80,17 +89,6 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     public void shelterInfo(Long chatId, Update update) { // кнопки этапа 1
 
-        CallbackQuery callbackQuery = update.callbackQuery();
-        String data = callbackQuery.data();
-        switch (data) {
-            case "инфа о приюте" ->
-                    telegramBot.execute(new SendMessage(chatId, "тут должна быть информация о приюте"));
-            case "расписание работы" ->
-                    telegramBot.execute(new SendMessage(chatId, " тут должно быть расписание работы приюта и адрес, схему проезда"));
-            case "рекомендации о ТБ" ->
-                    telegramBot.execute(new SendMessage(chatId, "тут должны быть общие рекомендации о технике безопасности на территории приюта"));
-        }
-
         SendMessage message = new SendMessage(chatId, "Приветствует в нашем приюте");
 
         InlineKeyboardButton button1 = new InlineKeyboardButton("инфа о приюте");
@@ -103,12 +101,14 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         button4.callbackData("записать данные");
         InlineKeyboardButton button5 = new InlineKeyboardButton("Позвать волонтера");
         button5.callbackData("4");
-        Keyboard keyboard = new InlineKeyboardMarkup(button1, button2, button3,button4,button5);
+        InlineKeyboardMarkup keyboard = new InlineKeyboardMarkup();
+        keyboard.addRow(button1);
+        keyboard.addRow(button2);
+        keyboard.addRow(button3);
+        keyboard.addRow(button4);
+        keyboard.addRow(button5);
         message.replyMarkup(keyboard);
         telegramBot.execute(message);
-
-
-
     }
 
 }
